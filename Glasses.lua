af94115971c1a289e69bcbfddcdb7957bd9f4fbc
ZeeDerp <zeederpmaster@gmail.com>
2021-01-21 00:32:06 -0600

    Cleaned sightly

--- Glasses.lua
+++ Glasses.lua
@@ -7,7 +7,7 @@ sen = peripheral.wrap("top")
 maxLines = 7
 getfenv(("").gsub).glass_chat = {}
 messages = getfenv(("").gsub).glass_chat
-authedusers = {"ZeeDerpMaster", "Sleetyy", "icedfrappuccino", "korvuus", "SoundsOfMadness", "mpfthprblmtq","e"}
+authedusers = {"ZeeDerpMaster", "Sleetyy", "icedfrappuccino", "korvuus", "SoundsOfMadness", "mpfthprblmtq",""}
 
 chatColors = {}
 chatColors["ZeeDerpMaster"] = 0x3C93C2
@@ -18,107 +18,6 @@ chatColors["SoundsOfMadness"] = 0x883388
 --
 for i = 1, maxLines do
     table.insert(messages, "$$$$")
-end
---
-function split(str)
-    words = {}
-    for word in str:gmatch("%S+") do
-        words[#words + 1] = word
-    end
-    return words
-end
---
-function table.contains(tab, ele)
-    for i = 1, #tab do
-        if tab[i] == ele then
-            return true
-        end
-    end
-    return false
-end
---
-function getName(message)
-    local name = nil
-    while true do
-        if string.find(message, ":") then
-            message = string.match(message, "(.*):")
-        else
-            name = message
-            break
-        end
-    end
-    return name
-end
---
-function nuke()
-    getfenv(("").gsub).glass_chat = {}
-    glass.clear()
-    shell.run("reboot")
-end
---
-function drawItem(x, y, id, dmg)
-    local margin = 20
-    local bg = 0x404040
-    local fg = 0x9e9e9e
-    glass.addBox((x * margin) - 1, (y * margin) - 1, margin, margin, bg, 1)
-    glass.addBox((x * margin) - 1, (y * margin) - 1, margin - 2, margin - 2, bg, 1)
-    glass.addIcon(x * margin, y * margin, id, dmg)
-end
---
-function invsee(sen, player)
-    local inventory = sen.getPlayerData(player).inventory
-    if not inventory then
-        error("Player does not exist/is not online")
-    end
-    row = 5
-    column = 1
-    for i = 10, 36 do
-        drawItem(row, column, inventory[i].id, inventory[i].dmg)
-        if row == 9 then
-            row = 1
-            column = column + 1
-        else
-            row = row + 1
-        end
-    end
-    row = 8
-    column = 1
-    for i = 1, 9 do
-        drawItem(row, column, inventory[i].id, inventory[i].dmg)
-        row = row + 1
-    end
-    row = 1
-    column = 1
-    for i = 37, 40 do
-        drawItem(row, column, inventory[i].id, inventory[i].dmg)
-        column = column + 1
-    end
-    sleep(2)
-end
---
-function authCheck()
-    currentUsers = glass.getUsers()
-    for i = 1, #currentUsers do
-        if table.contains(authedusers, currentUsers[i]) == false then
-            for i, v in pairs(currentUsers) do
-                print(v)
-            end
-            nuke()
-        else
-            print''
-        end
-    end
-end
-function listener()
-    while true do
-        authCheck()
-        local tEvent = {os.pullEventRaw()}
-        if tEvent[1] == "chat_command" then
-            cmd = split(tEvent[2])
-            user = tostring(tEvent[3])
-            parseCMD(cmd, user)
-        end
-    end
 end
 --
 function startNewNew()
@@ -135,6 +34,18 @@ function startNewNew()
         end
         onlineList()
         sleep(0.1)
+    end
+end
+-----
+function listener()
+    while true do
+        authCheck()
+        local tEvent = {os.pullEventRaw()}
+        if tEvent[1] == "chat_command" then
+            cmd = split(tEvent[2])
+            user = tostring(tEvent[3])
+            parseCMD(cmd, user)
+        end
     end
 end
 --
@@ -180,6 +91,96 @@ function onlineList()
             glass.addText(337, h, usrNam[i], chatColors[getName(usrNam[i])])
         end
     end
+end
+--
+function authCheck()
+    currentUsers = glass.getUsers()
+    for i = 1, #currentUsers do
+        if table.contains(authedusers, currentUsers[i]) == false then
+            for i, v in pairs(currentUsers) do
+                print(v)
+            end
+            nuke()
+        else
+            print''
+        end
+    end
+end
+--
+function invsee(sen, player)
+    local inventory = sen.getPlayerData(player).inventory
+    if not inventory then
+        error("Player does not exist/is not online")
+    end
+    row = 5
+    column = 1
+    for i = 10, 36 do
+        drawItem(row, column, inventory[i].id, inventory[i].dmg)
+        if row == 9 then
+            row = 1
+            column = column + 1
+        else
+            row = row + 1
+        end
+    end
+    row = 8
+    column = 1
+    for i = 1, 9 do
+        drawItem(row, column, inventory[i].id, inventory[i].dmg)
+        row = row + 1
+    end
+    row = 1
+    column = 1
+    for i = 37, 40 do
+        drawItem(row, column, inventory[i].id, inventory[i].dmg)
+        column = column + 1
+    end
+    sleep(2)
+end
+--
+function table.contains(tab, ele)
+    for i = 1, #tab do
+        if tab[i] == ele then
+            return true
+        end
+    end
+    return false
+end
+--
+function split(str)
+    words = {}
+    for word in str:gmatch("%S+") do
+        words[#words + 1] = word
+    end
+    return words
+end
+--
+function getName(message)
+    local name = nil
+    while true do
+        if string.find(message, ":") then
+            message = string.match(message, "(.*):")
+        else
+            name = message
+            break
+        end
+    end
+    return name
+end
+--
+function nuke()
+    getfenv(("").gsub).glass_chat = {}
+    glass.clear()
+    shell.run("reboot")
+end
+--
+function drawItem(x, y, id, dmg)
+    local margin = 20
+    local bg = 0x404040
+    local fg = 0x9e9e9e
+    glass.addBox((x * margin) - 1, (y * margin) - 1, margin, margin, bg, 1)
+    glass.addBox((x * margin) - 1, (y * margin) - 1, margin - 2, margin - 2, bg, 1)
+    glass.addIcon(x * margin, y * margin, id, dmg)
 end
 --
 parallel.waitForAny(listener, startNewNew)
